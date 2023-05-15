f1a609384657e8858c751ca82400762a472d13eb
e <amha17@student.aau.dk>
2023-05-15 12:57:32 +0200

    get time testing

--- Main.py
+++ Main.py
@@ -15,6 +15,7 @@ if __name__ == "__main__":
 	from tqdm import tqdm
 	from multiprocessing import freeze_support
 	import shutil
+	import tracemalloc
 
 	import torchvision.datasets as datasets
 	import torchvision.models as models
@@ -154,6 +155,10 @@ if __name__ == "__main__":
 		if(os.path.exists(expert_dir)):
 			shutil.rmtree(expert_dir)
 
+		# start monitoring the time and memory usage
+		trainStart = datetime.now()
+		tracemalloc.start()
+
 		#start training
 		for task_number in range(1, opt.no_of_tasks+1):
 	
@@ -235,6 +240,25 @@ if __name__ == "__main__":
 					train_model(len(image_folder.classes), feature_extractor, encoder_criterion, dset_loaders, dset_size, opt.num_epochs_model, cuda, task_number, relatedness_info, opt, lr = opt.lr)
 				if opt.approach == "consoligate":
 					train_model_consolidate(len(image_folder.classes), feature_extractor, encoder_criterion, dset_loaders, dset_size, opt.num_epochs_model, cuda, task_number, relatedness_info, opt, lr = opt.lr)
+					
+		print("Peak memory usage in training: " + str(tracemalloc.get_traced_memory()))
+		tracemalloc.stop()
+		trainEnd = datetime.now()
+		trainTime = (trainEnd - trainStart).total_seconds()
+		print("Training time: " + "{:.0f}".format(trainTime) + " seconds")
 
 	if opt.mode == "test" or opt.mode == "run":
+		# start monitoring the memory usage and time
+		tracemalloc.start()
+		testStart = datetime.now()
+
+
 		test_models(opt)
+
+		
+		testEnd = datetime.now()
+		testTime = (testEnd - testStart).total_seconds()
+		print("Testing time: " + "{:.0f}".format(testTime) + " seconds")
+
+		print("\n Highest memory usage in testing:" + tracemalloc.get_traced_memory())
+		tracemalloc.stop()
